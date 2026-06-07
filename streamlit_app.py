67ff24784c92b831b24922a2134c46ed2108383f
wjsalswns11444-debug <wjsalswns11444@gmail.com>
2026-06-08 07:19:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("AI 기반 logD 예측 및 구조 최적화 시스템")
+
+smiles = st.text_input("SMILES를 입력하세요", "CCO")
+
+if st.button("logD 예측 및 구조 추천"):
+    st.write("입력한 SMILES:", smiles)
+    st.metric("예측 logD", "2.35")
+    st.success("목표 logD 범위(1~3)에 포함됩니다.")
